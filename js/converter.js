1400dee14b8fac4f07a5d91f587c052e0fa0ff58
Gokay Kayki <gokaykayki@gmail.com>
2017-12-31 23:35:01 +0300

    added binance support

--- js/converter.js
+++ js/converter.js
@@ -4,11 +4,31 @@ $(document).ready(function(){
   //Variable definitions
   var to = "" // target currency
   var buttonC = document.createElement("button"); //converter button
-  var balanceCol = $("#balance-wrapper #pad-wrapper .row:first .col-lg-12 .row:nth-child(2)"); //the place where the button will be added
-  var estimatedDiv = balanceCol.find("h4"); //the tag that contains the balance
-
+  var site = $(location).attr('hostname');
+  var balanceCol = "";
+  var estimatedDiv = "";
+  var estimated = "";
+  var fSplit = "";
+  var from = "";
+  var to = "";
+  var result = "";
+  var amount = "";
+  var localAmount = "";
+  var rSplit = "";
+  var parseAmount = "";
+  var appendString = "";
+  var url = "https://finance.google.com/finance/converter?a="; // pre
   buttonC.innerHTML = chrome.i18n.getMessage("buttonText"); //set button text
-  buttonC.className = "converterButton"; //add class to button for css
+
+  if(site == "www.bittrex.com" || site == "bittrex.com"){
+    balanceCol = $("#balance-wrapper #pad-wrapper .row:first .col-lg-12 .row:nth-child(2)"); //the place where the button will be added
+    estimatedDiv = balanceCol.find("h4"); //the tag that contains the balance
+    buttonC.className = "cbBittrex convertButton"; //add class to button for css
+  }else if(site == "www.binance.com"|| site == "binance.com"){
+    balanceCol = $("li.total"); //the place where the button will be added
+    estimatedDiv = balanceCol.find("strong.ng-scope"); //the tag that contains the balance
+    buttonC.className = "cbBinance convertButton"; //add class to button for css
+  }
 
   balanceCol.append(buttonC); //place the button
 
@@ -21,13 +41,8 @@ $(document).ready(function(){
     }
   });
 
-  $(balanceCol).on('click', ".converterButton", function(){
-    var estimated = balanceCol.find("h4").html(); // get total balance
-    var fSplit = estimated.split(" ");
-    var from = fSplit[6]; // displayed currency (this can be removed soon)
-    var amount = fSplit[5]; // balance amount
-    var result = '';
-    var url = "https://finance.google.com/finance/converter?a=" + amount + "&from=" + from + "&to=" + to;
+  function convert(){
+    url = url + amount + "&from=" + from + "&to=" + to;
     $.ajaxSetup({async: false});
     $.get(url, function (data) {
       // parse the page
@@ -38,12 +53,54 @@ $(document).ready(function(){
         result = result.replace('<div id=currency_converter_result>', '');
         result = result.replace('<span class=bld>', '');
         result = result.replace('</span>', '');
-        var rSplit = result.split(" = "); // we got the converted amount and currency on rSplit[1]
-        if(fSplit.length < 8){
-          estimatedDiv.append(" / " + rSplit[1]); // add converted amount and currency
-        }
+        rSplit = result.split(" = "); // we got the converted amount and currency on rSplit[1]
+        parseAmount = rSplit[1];
       }
     });
+  }
+
+  function bittrex(){
+    estimated = balanceCol.find("h4").html(); // get total balance
+    fSplit = estimated.split(" ");
+    from = fSplit[6]; // displayed currency (this can be removed soon)
+    amount = fSplit[5]; // balance amount
+    convert();
+    if( amount == "0.00"){
+      appendString = " / 0 " + to;
+    }else{
+      appendString = " / " + parseAmount;
+    }
+    if(fSplit.length < 8){
+      estimatedDiv.append(appendString); // add converted amount and currency
+    }
+  }
+
+  function binance(){
+    estimated = estimatedDiv.text(); // get total balance
+    from = estimated.charAt(0); // displayed currency (this can be removed soon)
+    if( from == "$"){
+      from = "USD";
+      amount = estimated.slice(1,estimated.length); // balance amount
+      convert();
+      if( amount == "0.00"){
+        appendString = " / 0 " + to;
+      }else{
+        appendString = " / " + parseAmount;
+      }
+      if(estimated.indexOf("/") == -1){
+        estimatedDiv.append(appendString);
+      }
+    } else{
+      estimatedDiv.append(" / Currency error"); // add error
+    }
+  }
+
+  $(balanceCol).on('click', ".convertButton", function(){
+    if(site == "www.bittrex.com" || site == "bittrex.com"){
+      bittrex();
+    }else if(site == "www.binance.com" || site == "binance.com"){
+      binance();
+    }
   });
 
 });
